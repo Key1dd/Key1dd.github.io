7bc09f78f1c6650437644ace7a1c9a6d2ae42f55
Keyi <32367087+Key1dd@users.noreply.github.com>
2017-11-30 03:29:09 -0500

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -4,19 +4,26 @@ function prepareGallery(){
    if(!document.gerElementById("imagegallery")) return false;                           
  //check getElementsByTage and getElementById methods 
  var gallery = document.getElementById("imagegallery");
- var links = gallery.getElementById("a");
+ var links = gallery.getElementsByTagName("a");
  for (var i = 0; i < links.length; i++){
+    
   links[i].onclick = function(){
-   showPic(this);
-   return false;
+   return showPic(this) ? false:true;
    }
   }
  }
 function showPic(whichpic) { 
+   if(!document.getElementById("placeholder")) return false;
             var source = whichpic.getAttribute("href");
             var placeholder = document.getElementById("placeholder");
+   if(palceholder.nodeName != "IMG") return false;
             placeholder.setAttribute("src", source);
-            var text = whichpic.getAttribute("title");
+   if(document.getElementById("description")){
+            var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title"): "";
             var description = document.getElementById("description");
+      if(description.firstChild.nodeType == 3){
             description.firstChild.nodeValue = text;
         }
+      }
+   return true;
+   }
