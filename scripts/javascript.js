624ef7b826f27f8ccba0ecf06dcb13bbc9a35d54
Keyi <32367087+Key1dd@users.noreply.github.com>
2017-11-30 03:20:03 -0500

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,18 @@
- function showPic(whichpic) { 
+function prepareGallery(){
+   if(!document.getElementsByTagName) return false;
+   if(!document.getElementById) return false;
+   if(!document.gerElementById("imagegallery")) return false;                           
+ //check getElementsByTage and getElementById methods 
+ var gallery = document.getElementById("imagegallery");
+ var links = gallery.getElementById("a");
+ for (var i = 0; i < links.length; i++){
+  links[i].onclick = function(){
+   showPic(this);
+   return false;
+   }
+  }
+ }
+function showPic(whichpic) { 
             var source = whichpic.getAttribute("href");
             var placeholder = document.getElementById("placeholder");
             placeholder.setAttribute("src", source);
