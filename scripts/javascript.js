2607abd80166d03429e9e2e7a7183a9d2c8bbf2f
Keyi <32367087+Key1dd@users.noreply.github.com>
2017-12-01 00:28:43 -0500

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,3 +1,55 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+function insertAfter(newElement,targetElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement,targetElement.nextSibling);
+  }
+}
+
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var placeholder = document.createElement("img");
+  placeholder.setAttribute("id","placeholder");
+  placeholder.setAttribute("src","images/placeholder.gif");
+  placeholder.setAttribute("alt","my image gallery");
+  var description = document.createElement("p");
+  description.setAttribute("id","description");
+  var desctext = document.createTextNode("Choose an image");
+  description.appendChild(desctext);
+  var gallery = document.getElementById("imagegallery");
+  insertAfter(placeholder,gallery);
+  insertAfter(description,placeholder);
+}
+
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for ( var i=0; i < links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this);
+	}
+    links[i].onkeypress = links[i].onclick;
+  }
+}
 
 function showPic(whichpic) {
   if (!document.getElementById("placeholder")) return true;
@@ -17,30 +69,5 @@ function showPic(whichpic) {
   return false;
 }
 
-function prepareGallery() {
-  if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;
-  if (!document.getElementById("imagegallery")) return false;
-  var gallery = document.getElementById("imagegallery");
-  var links = gallery.getElementsByTagName("a");
-  for ( var i=0; i < links.length; i++) {
-    links[i].onclick = function() {
-      return showPic(this);
-	}
-    links[i].onkeypress = links[i].onclick;
-  }
-}
-
-function addLoadEvent(func) {
-  var oldonload = window.onload;
-  if (typeof window.onload != 'function') {
-    window.onload = func;
-  } else {
-    window.onload = function() {
-      oldonload();
-      func();
-    }
-  }
-}
-
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
